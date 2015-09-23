42899ce3b99224b49d00a49519e82bd4f2a88c95
Schaemelhout <Mathias015@hotmail.com>
2015-09-23 14:04:30 +0200

    no message

---
+++ src/scripts/directives/iconbutton.js
@@ -0,0 +1,22 @@
+'use strict';
+(function (module) {
+  try {
+    module = angular.module('awelzijn.directives');
+  } catch (e) {
+    module = angular.module('awelzijn.directives', []);
+  }
+  module.directive('aWelzijnButton', [function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      scope: {
+        icon: '@',
+        type: '@',
+        ngClick: '&'
+      },
+      template: '<a class="btn icon-btn btn-{{type}}"><i class="fa {{icon}}"></i><span ng-transclude/></a>',
+      link: function (scope, element, attrs) { }
+    };
+  }]);
+})();
