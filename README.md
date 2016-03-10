8c88027f4730d0039c34f38cbd99139c8ead2125
Mathias Schaemelhout <mathias.schaemelhout@digipolis.be>
2016-03-10 11:42:35 +0100

    Release v1.0.27

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # A-Welzijn Directives
 
-v1.0.26
+v1.0.27
 
 ### Hoe het te gebruiken
 
--- src/scripts/directives/klantfiche.js
+++ src/scripts/directives/klantfiche.js
@@ -26,9 +26,9 @@
 
 				ctrl.popoverOpened = function () {
 					ctrl.loading = true;
-					var httpConfig = { method: 'GET', url: appConfig.welzijnKlant + 'dossier/' + ctrl.sleutel };
-					$http(httpConfig).then(function (data) {
-						ctrl.dossier = data.data.klant;
+					var httpConfig = { method: 'GET', url: appConfig.apiRoot + "klant/dossier?sleutel=" + ctrl.sleutel };
+					$http(httpConfig).then(function (response) {
+						ctrl.dossier = response.data.klantDossier;
 					}, function (data) { }).finally(function () {
 						ctrl.loading = false;
 					});
